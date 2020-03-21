e306b03cf291e41f8b10c4220b7ca334f3daa1a9
Serhio <Samodurov_sm@mail.ru>
2020-03-21 20:33:40 +0300

    Добавление скрипта по созданию БД таблиц и их заполнению

---
+++ lesson02/CREATE_bd_tb.sql
@@ -0,0 +1,27 @@
+-- DROP DATABASE IF EXISTS example;
+CREATE DATABASE IF NOT EXISTS example;
+USE example;
+
+-- DROP TABLE IF EXISTS users
+CREATE TABLE IF NOT EXISTS users (
+  id SERIAL PRIMARY KEY,
+  name VARCHAR(255) COMMENT 'Имя ользователя',
+  created_at DATETIME DEFAULT CURRENT_TIMESTAMP
+ ) COMMENT = 'Пользователи';
+ 
+
+ INSERT INTO users VALUES
+        (DEFAULT, 'Max', DEFAULT),
+        (DEFAULT, 'Leo', DEFAULT),
+        (DEFAULT, 'Kate', DEFAULT),
+        (DEFAULT, 'Igor', DEFAULT),
+        (DEFAULT, 'Ragnar', DEFAULT),
+        (DEFAULT, 'Geralt', DEFAULT),
+        (DEFAULT, 'Rapuncel', DEFAULT),
+        (DEFAULT, 'Irina', DEFAULT),
+        (DEFAULT, 'Viktor', DEFAULT),
+        (DEFAULT, 'Bob', DEFAULT),
+        (DEFAULT, 'Jane', DEFAULT);
+
+-- SELECT * FROM users;
+SELECT * FROM users LIMIT 5;
