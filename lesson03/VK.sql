bdaef7907618b2b11a763d1ac9ba24a6c423f03c
Serhio <Samodurov_sm@mail.ru>
2020-03-30 18:20:46 +0300

    Домашнее задание 3

---
+++ lesson03/VK.sql
@@ -0,0 +1,84 @@
+CREATE DATABASE VKontakte;
+USE VKontakte;
+
+CREATE TABLE users (
+  id INT UNSIGNED NOT NULL AUTO_INCREMENT PRIMARY KEY,
+  first_name VARCHAR(100) NOT NULL,
+  last_name VARCHAR(100) NOT NULL,
+  email VARCHAR(120) NOT NULL UNIQUE,
+  phone VARCHAR(10) NOT NULL UNIQUE,
+  created_at DATETIME DEFAULT NOW(),
+  updated_at DATETIME DEFAULT NOW() ON UPDATE NOW()
+);
+
+
+CREATE TABLE profiles (
+  user_id INT UNSIGNED NOT NULL PRIMARY KEY,
+  gender CHAR(1) NOT NULL,
+  birthday DATE,
+  city VARCHAR(100),
+  country VARCHAR(100),
+  photo_id INT UNSIGNED,
+  created_at DATETIME DEFAULT NOW(),
+  updated_at DATETIME DEFAULT NOW() ON UPDATE NOW()
+);
+
+
+CREATE TABLE messages (
+  id INT UNSIGNED NOT NULL AUTO_INCREMENT PRIMARY KEY,
+  from_user_id INT UNSIGNED NOT NULL,
+  to_user_id INT UNSIGNED NOT NULL,
+  body TEXT NOT NULL,
+  is_important BOOLEAN,
+  is_delivered BOOLEAN,
+  created_at DATETIME DEFAULT NOW()
+);
+
+
+CREATE TABLE friendship (
+  user_id INT UNSIGNED NOT NULL,
+  friend_id INT UNSIGNED NOT NULL,
+  status_id INT UNSIGNED NOT NULL,
+  requested_at DATETIME DEFAULT NOW(),
+  confirmed_at DATETIME DEFAULT NOW() ON UPDATE NOW(), -- добавил время апдейта автоматически
+  PRIMARY KEY (user_id, friend_id)
+);
+
+
+CREATE TABLE friendship_statuses (
+  id INT UNSIGNED NOT NULL AUTO_INCREMENT PRIMARY KEY,
+  name VARCHAR(150) NOT NULL UNIQUE
+);
+
+
+CREATE TABLE communities (
+  id INT UNSIGNED NOT NULL AUTO_INCREMENT PRIMARY KEY,
+  name VARCHAR(150) NOT NULL UNIQUE,
+  created_at DATETIME DEFAULT NOW()
+);
+
+
+CREATE TABLE communities_users (
+  community_id INT UNSIGNED NOT NULL,
+  user_id INT UNSIGNED NOT NULL,
+  PRIMARY KEY (community_id, user_id)
+);
+
+
+CREATE TABLE media (
+  id INT UNSIGNED NOT NULL AUTO_INCREMENT PRIMARY KEY,
+  media_type_id INT UNSIGNED NOT NULL,
+  user_id INT UNSIGNED NOT NULL,
+  filename VARCHAR(255) NOT NULL,
+  size INT NOT NULL,
+  metadata JSON,
+  created_at DATETIME DEFAULT CURRENT_TIMESTAMP,
+  updated_at DATETIME DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP
+);
+
+
+CREATE TABLE media_types (
+  id INT UNSIGNED NOT NULL AUTO_INCREMENT PRIMARY KEY,
+  name VARCHAR(255) NOT NULL UNIQUE
+);
+
